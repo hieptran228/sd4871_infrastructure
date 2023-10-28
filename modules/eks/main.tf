2e5e70574da87a3e1375f1e71eadd84079f9b7e5
Tran Hoang Hiep <hiepgia@gmail.com>
2023-10-29 02:41:04 +0700

    Add sidecar ingress rules

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -27,6 +27,25 @@ module "eks" {
   eks_managed_node_groups = var.node_groups
 
   tags = module.eks_tags.tags
+
+  node_security_group_additional_rules = {
+    ingress_15017 = {
+      description                   = "Cluster API - Istio Webhook namespace.sidecar-injector.istio.io"
+      protocol                      = "TCP"
+      from_port                     = 15017
+      to_port                       = 15017
+      type                          = "ingress"
+      source_cluster_security_group = true
+    }
+    ingress_15012 = {
+      description                   = "Cluster API to nodes ports/protocols"
+      protocol                      = "TCP"
+      from_port                     = 15012
+      to_port                       = 15012
+      type                          = "ingress"
+      source_cluster_security_group = true
+    }
+  }
 }
 
 
